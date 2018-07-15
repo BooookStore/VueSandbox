8f2c644d952af4f534706d57fa33d6b64a226df3
bookstore <booookstore@icloud.com>
2018-07-15 11:35:34 +0900

    :art: フォーマットを変更

--- src/components/organisms/global/OrganismsHeader.vue
+++ src/components/organisms/global/OrganismsHeader.vue
@@ -5,15 +5,15 @@
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
+    import {Component, Vue} from 'vue-property-decorator';
 
-import MoleculeLogoLink from '@/components/molecules/media/MoleculeLogoLink';
+    import MoleculeLogoLink from '@/components/molecules/media/MoleculeLogoLink';
 
-@Component({
-    components: {MoleculeLogoLink},
-})
-export default class OrganismsHeader extends Vue {
-}
+    @Component({
+        components: {MoleculeLogoLink},
+    })
+    export default class OrganismsHeader extends Vue {
+    }
 </script>
 
 <style scoped>
