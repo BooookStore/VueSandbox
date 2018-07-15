6a2eb6fa07db94f45e90add084dbff5df6a4bcaf
bookstore <booookstore@icloud.com>
2018-07-15 13:41:35 +0900

    :sparkles: ロゴを作成

--- src/components/molecules/media/MoleculeLogoLink.vue
+++ src/components/molecules/media/MoleculeLogoLink.vue
@@ -1,9 +1,14 @@
 <template>
-    <div>
-        <i class="pencil alternate icon"></i> Vue Sandbox Project
+    <div id="root">
+        <i class="pencil alternate icon"></i>
+        <span>Vue Sandbox Project</span>
     </div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+    #root {
+        display: inline-block;
+        padding: 5px;
+        font-size: 18px;
+    }
 </style>
