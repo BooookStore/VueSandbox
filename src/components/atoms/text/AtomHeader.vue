f8c7d540290c42b32652cc30502abfd6a587f310
bookstore <booookstore@icloud.com>
2018-07-21 11:54:56 +0900

    feat : ヘッダーコンポーネントを作成

---
+++ src/components/atoms/text/AtomHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="header">
+        <slot/>
+    </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component({})
+export default class AtomHeader extends Vue {
+    // todo : heading level can specifiable.
+}
+</script>
