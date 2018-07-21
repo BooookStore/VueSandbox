b866e8cbbec19e89e6a44b11dd2586ac225953f9
bookstore <booookstore@icloud.com>
2018-07-21 11:36:26 +0900

    feat : 商品イメージのAtomを作成

---
+++ src/components/atoms/image/AtomProductImage.vue
@@ -0,0 +1,12 @@
+<template>
+    <img :src="src"/>
+</template>
+
+<script lang="ts">
+import {Vue, Component, Prop} from 'vue-property-decorator';
+
+@Component({})
+export default class AtomProductImage extends Vue {
+    @Prop(String) public src: string;
+}
+</script>
